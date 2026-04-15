856c79159528a5c9323264bb1e7ee1f3880227c8
GiorgiaFormicola <giorgia.formicola97@gmail.com>
2026-04-15 14:58:30 +0200

    Adds AuthorsController

---
+++ src/main/java/GiorgiaFormicola/U5_W2_D3/controllers/AuthorsController.java
@@ -0,0 +1,9 @@
+package GiorgiaFormicola.U5_W2_D3.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/authors")
+public class AuthorsController {
+}
