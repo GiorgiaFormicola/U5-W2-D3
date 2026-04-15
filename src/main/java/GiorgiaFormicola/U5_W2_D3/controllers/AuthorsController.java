45992a6e77e6663a55624d7f33ea46d79f6d815a
GiorgiaFormicola <giorgia.formicola97@gmail.com>
2026-04-15 15:43:26 +0200

    Adds saveAuthor() funcionality

--- src/main/java/GiorgiaFormicola/U5_W2_D3/controllers/AuthorsController.java
+++ src/main/java/GiorgiaFormicola/U5_W2_D3/controllers/AuthorsController.java
@@ -1,9 +1,22 @@
 package GiorgiaFormicola.U5_W2_D3.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import GiorgiaFormicola.U5_W2_D3.entities.Author;
+import GiorgiaFormicola.U5_W2_D3.payloads.AuthorPayload;
+import GiorgiaFormicola.U5_W2_D3.services.AuthorsService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/authors")
+@AllArgsConstructor
 public class AuthorsController {
+    private AuthorsService authorsService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Author saveAuthor(@RequestBody AuthorPayload body) {
+        return this.authorsService.saveAuthor(body);
+    }
+
 }
--- src/main/java/GiorgiaFormicola/U5_W2_D3/services/AuthorsService.java
+++ src/main/java/GiorgiaFormicola/U5_W2_D3/services/AuthorsService.java
@@ -1,11 +1,25 @@
 package GiorgiaFormicola.U5_W2_D3.services;
 
+import GiorgiaFormicola.U5_W2_D3.entities.Author;
+import GiorgiaFormicola.U5_W2_D3.exceptions.BadRequestException;
+import GiorgiaFormicola.U5_W2_D3.payloads.AuthorPayload;
 import GiorgiaFormicola.U5_W2_D3.repositories.AuthorsRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @AllArgsConstructor
 public class AuthorsService {
     private AuthorsRepository authorsRepository;
+
+    public Author saveAuthor(AuthorPayload body) {
+        if (this.authorsRepository.existsByEmail(body.getEmail()))
+            throw new BadRequestException("Email address " + body.getEmail() + " already in use!");
+        Author newAuthor = new Author(body.getName(), body.getSurname(), body.getEmail(), body.getBirthDate());
+        Author savedAuthor = authorsRepository.save(newAuthor);
+        log.info("Author with id " + savedAuthor.getId() + " successfully saved");
+        return savedAuthor;
+    }
 }
