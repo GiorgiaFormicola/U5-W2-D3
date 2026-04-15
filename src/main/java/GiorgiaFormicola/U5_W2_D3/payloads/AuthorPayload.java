685736ba0033238e8710ad3edc2aee459eaab09b
GiorgiaFormicola <giorgia.formicola97@gmail.com>
2026-04-15 15:01:11 +0200

    Adds AuthorPayload class

---
+++ src/main/java/GiorgiaFormicola/U5_W2_D3/payloads/AuthorPayload.java
@@ -0,0 +1,10 @@
+package GiorgiaFormicola.U5_W2_D3.payloads;
+
+import java.time.LocalDate;
+
+public class AuthorPayload {
+    private String name;
+    private String surname;
+    private String email;
+    private LocalDate birthDate;
+}
